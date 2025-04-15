fc10eb49fab5fa6a816ed774e0e66cdf94ff5efb
cdhft12 <cdhhn12@gmail.com>
2025-04-15 16:10:20 +0900

    commit

---
+++ src/main/java/com/std/stdmall/StdmallApplication.java
@@ -0,0 +1,13 @@
+package com.std.stdmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/std/stdmall/StdmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.std.stdmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
